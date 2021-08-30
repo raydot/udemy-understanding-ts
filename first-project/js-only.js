0a9ea5d5b4ca2d5416fcef891f6362ee7d982484
Dave Kanter <moatgoat@hotmail.com>
2021-08-30 11:49:06 -0700

    Complete section 1.4

---
+++ first-project/js-only.js
@@ -0,0 +1,17 @@
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1");
+// const input2 = document.getElementById("num2");
+
+// // one way to solve the problem
+// // function add(num1, num2) {
+// //   if (typeof num1 === "number" && typeof num2 === "number") {
+// //     return num1 + num2;
+// //   } else {
+// //     // cast
+// //     return +num1 + +num2;
+// //   }
+// // }
+
+// button.addEventListener("click", function () {
+//   console.log(add(input1.value, input2.value));
+// });
